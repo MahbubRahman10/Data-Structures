ba576f0c1c31f918b8c413618d6d9fb22837b624
Mahbub Rahman <Mahbub.shuvo10@gmail.com>
2017-09-14 15:33:58 +0600

    Craete an Arrays in C

---
+++ Array/Create an Array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int i,n,newelement,position;
+int data[MAX_SIZE];
+
+int main(){
+
+    printf("Enter array elements : ");
+    scanf("%d",&n);
+
+    printf("Enter element to be insert : \n");
+    for(i=0;i<n;i++){
+        scanf("%d",&data[i]);
+    }
+
+    print();
+
+}
+
+void print(){
+    printf("Element in the Array : ");
+    for(i=0;i<n;i++){
+        printf("%d\t",data[i]);
+    }
+    printf("\n");
+}

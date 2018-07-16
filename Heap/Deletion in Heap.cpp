d2588f10c45cf8e2a4a733ba01a2b855e5ca808e
Mahbub <mahbub.shuvo10@gmail.com>
2018-07-16 21:41:34 +0600

    Deletion in Heap

---
+++ Heap/Deletion in Heap.cpp
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<iostream>
+#define MAX_SIZE 10
+
+using namespace std;
+
+int arrays[MAX_SIZE];
+int counts = 0;
+
+int adjust(int counts){
+
+    int lst = arrays[counts];
+    int i = counts;
+    while(i/2 >= 1){
+        if(arrays[i/2] < lst){
+            int temp = arrays[i];
+            arrays[i] = arrays[i/2];
+            arrays[i/2] = temp;
+            i = i/2;
+        }
+        else{
+            break;
+        }
+    }
+}
+
+int inserts(int value){
+
+    if(counts < MAX_SIZE-1){
+        counts++;
+        arrays[counts] = value;
+        adjust(counts);
+    }
+}
+
+
+
+int adjust_removes(int counts){
+
+    int i = 1*2;
+    while(i <= counts){
+        if(arrays[i+1] > arrays[i]){
+            i = i+1;
+        }
+        else if(arrays[i] < arrays[i/2] ){
+            break;
+        }
+        else{
+            swap(arrays[i],arrays[i/2]);
+            i = i*2;
+        }
+    }
+}
+
+int removes(){
+
+    swap(arrays[counts],arrays[1]);
+    counts--;
+    adjust_removes(counts);
+
+}
+
+
+int print(){
+
+    for(int i=1; i<=counts;i++){
+        cout << arrays[i] << " ";
+    }
+}
+
+int main(){
+
+    inserts(50);
+    inserts(30);
+    inserts(35);
+    inserts(5);
+    inserts(29);
+
+    print();
+
+    removes();
+    cout << "\n\n" << "After Remove"  << "\n";
+
+    print();
+
+}
+
